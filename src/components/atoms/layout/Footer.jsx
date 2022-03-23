135e7bffe7600106392b9c9f95addfe84990335f
hirom <hirom0808@gmail.com>
2022-03-23 21:11:28 +0900

    Initial commit

---
+++ src/components/atoms/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Footer = () => {
+  return (
+    <SFooter>
+
+    </SFooter>
+  );
+};
+
+const SFooter = styled.footer`
+  background-color: #11999e;
+  color: #fff;
+  text-align: center;
+  padding: 8px 0;
+`;
